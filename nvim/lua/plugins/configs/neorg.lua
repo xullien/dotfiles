af86ddcf42371f4a6d5612edbdeaf74ef8e426e3
xullien <adoni.khalil@gmail.com>
2023-08-11 09:25:30 +0200

    making neorg prettier

--- nvim/lua/plugins/configs/neorg.lua
+++ nvim/lua/plugins/configs/neorg.lua
@@ -1,7 +1,47 @@
 require('neorg').setup {
     load = {
         ["core.defaults"] = {},
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.ui"] = {},
+        ["core.itero"] = {},
+        ["core.concealer"] = {
+            config = {
+                icon_preset = "varied",
+                icons = {
+                    delimiter = {
+                        horizontal_line = {
+                            highlight = "@neorg.delimiters.horizontal_line",
+                        },
+                    },
+                    code_block = {
+                        -- If true will only dim the content of the code block (without the
+                        -- `@code` and `@end` lines), not the entirety of the code block itself.
+                        content_only = true,
+                        conceal = true,
+
+                        -- The width to use for code block backgrounds.
+                        --
+                        -- When set to `fullwidth` (the default), will create a background
+                        -- that spans the width of the buffer.
+                        --
+                        -- When set to `content`, will only span as far as the longest line
+                        -- within the code block.
+                        width = "content",
+
+                        -- Additional padding to apply to either the left or the right. Making
+                        -- these values negative is considered undefined behaviour (it is
+                        -- likely to work, but it's not officially supported).
+                        padding = {
+                            left = 1,
+                            right = 1,
+                        },
+
+                        nodes = { "ranged_verbatim_tag" },
+                        highlight = "CursorLine",
+                        insert_enabled = true,
+                    },
+                },
+            },
+        }, -- Adds pretty icons to your documents
         ["core.dirman"] = {
             config = {
                 workspaces = {
@@ -54,6 +94,17 @@ require('neorg').setup {
                 end,
             }
         }
+        -- ["core.highlights"] = {
+        --    config = {
+        --         highlights = {
+        --             markup = {
+        --                 bold = {
+        --                     delimiter = "+NonText",
+        --                 },
+        --             },
+        --         },
+        --     }, 
+        -- },
     }
 }
 
